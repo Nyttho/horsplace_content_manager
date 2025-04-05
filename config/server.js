cde34115b084b16356d431c2a071e01d6cb023e3
Nyttho <anthony.dms@outlook.com>
2025-04-05 10:33:08 +0200

    server change

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  url: env("PUBLIC_URL", ""),
+  admin: {
+    url: "/admin", // Configuration du chemin d'administration
+    autoOpen: false,
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
